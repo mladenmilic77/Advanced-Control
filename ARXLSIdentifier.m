284908cd738a31fb9ae6ea06fa96358a83c8c5ae
Mladen Milic <mladen180577@gmail.com>
2026-05-24 21:51:42 +0200

    Identifier finished version 1.0

--- ARXLSIdentifier.m
+++ ARXLSIdentifier.m
@@ -64,4 +64,97 @@ classdef ARXLSIdentifier < handle
             obj = ARXLSIdentifier(options);
         end
     end
+
+    methods (Access = public)
+        function yHat = Step(obj, u, y)
+            %STEP Perform one recursive least squares update.
+            %   Updates ARX parameter estimates using new input-output sample.
+            
+            arguments
+                obj 
+                u (1,1) double % System input
+                y (1,1) double % Measured output
+            end
+
+            % Regression vector
+            obj.phi = [-obj.yHistory; obj.uHistory];
+
+            % Output prediction
+            obj.yHat = obj.phi' * obj.theta;
+
+            % Prediction error
+            obj.error = y - obj.yHat;
+
+            % RLS adaption gain vector
+            gain = (obj.P * obj.phi)/(obj.lambda + obj.phi' * obj.P * obj.phi);
+
+            % Parameter estimates update
+            obj.theta = obj.theta + gain * obj.error;
+
+            % Covariance matrix update
+            obj.P = (1 / obj.lambda) * (obj.P - gain * (obj.phi' * obj.P));
+
+            % Update histories
+            obj.yHistory = [y; obj.yHistory(1:end-1)];
+            obj.uHistory = [u; obj.uHistory(1:end-1)];
+
+            % Return predicted output
+            yHat = obj.yHat;
+        end
+    
+        function Reset(obj)
+            %RESET Reset recursive least squares estimator.
+            %   Restores parameter estimates, covariance matrix,
+            %   prediction variables and input-output histories.
+
+            arguments
+                obj 
+            end
+
+            parameterCount = obj.na + obj.nb;
+            obj.theta = zeros(parameterCount, 1);
+            obj.phi = zeros(parameterCount, 1);
+            
+            obj.P = 1000 * eye(parameterCount);
+            
+            obj.yHat = 0;
+            obj.error = 0;
+            
+            obj.uHistory = zeros(obj.nb, 1);
+            obj.yHistory = zeros(obj.na, 1);
+        end
+        
+        function theta = GetParameters(obj)
+            %GETPARAMETERS Return estimated ARX parameter vector.
+            %   Returns current recursive least squares parameter estimates.
+
+            arguments
+                obj 
+            end
+
+            theta = obj.theta;
+        end
+
+        function yHat = GetPrediction(obj)
+            %GETPREDICTION Return latest predicted output.
+            %   Returns most recent ARX model output prediction.
+            
+            arguments
+                obj 
+            end
+
+            yHat = obj.yHat;
+        end
+
+        function error = GetError(obj)
+            %GETERROR Return latest prediction error.
+            %   Returns difference between measured and predicted output.
+
+            arguments
+                obj 
+            end
+
+            error = obj.error;
+        end
+    end
 end
